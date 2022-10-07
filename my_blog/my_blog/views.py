af5739d0c672d08058057a9f05db9f1ad2e7f032
Juan Pineda <jfpinedap+coder_43765@gmail.com>
2022-10-06 21:04:23 -0500

    la primera version de my_blog en la clase 17

---
+++ my_blog/my_blog/views.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+from django.http import HttpResponse
+
+
+def hello_world(request):
+    return HttpResponse("Hello this is my first view in Django!!")
+
+def title(request):
+    return HttpResponse("<h1>Este es un título en H1 </h1>")
+
+def my_name_is(request, name, age):
+    # edad = int(edad)
+    documentoDeTexto = f"Mi nombre es: <br><br>  {name} <br><br> Mi edad multiplicada por 2: {age*2}"
+    return HttpResponse(documentoDeTexto)
+
+def calculate_age(request, birth_day):
+    birth_day = datetime.strptime(birth_day, '%Y-%m-%d')
+    print(type(birth_day))
+    delta_time = datetime.now() - birth_day
+    days_by_year = 365.25
+
+    http_response = '''
+    <br><br>
+    I'm {years} years, {months} months, {days} days old.
+    '''.format(
+        years=int(delta_time.days // days_by_year),
+        months=int((delta_time.days % days_by_year) // 30),
+        days=int((delta_time.days % days_by_year) % 30),
+    )
+    return HttpResponse(http_response)
