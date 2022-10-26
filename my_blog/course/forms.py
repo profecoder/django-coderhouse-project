4aeb0a6d7a11cc32fd5ceb63f94f23cc8f05d190
Juan Pineda <jfpinedap+coder_43765@gmail.com>
2022-10-26 00:42:16 -0500

    Class 21 preparation

---
+++ my_blog/course/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+
+
+class CourseForm(forms.Form):
+    name = forms.CharField(
+        label="Nombre del curso",
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "class": "course-name",
+                "placeholder": "Nombre del curso",
+                "required": "True",
+            }
+        ),
+    )
+    code = forms.IntegerField(
+        label="Código:",
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                "class": "course-code",
+                "placeholder": "Código del curso",
+                "required": "True",
+            }
+        ),
+    )
--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -10,8 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,9 +59,7 @@ ROOT_URLCONF = 'my_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            os.path.join(BASE_DIR, 'templates')
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,3 +126,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-secondary',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
